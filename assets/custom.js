af855629564494629e740282fe6ef8ad1c1886db
ipanasiuk <ipanasiuk.1993@gmail.com>
2026-01-13 11:20:30 +0200

    Fixes in header group. Add announcement swiper slider

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,17 @@
-console.log("Output from custom JS");
+document.querySelectorAll('.announcement-bar__swiper').forEach((slider) => {
+  const speed = Number(slider.dataset.slider_speed);
+  const autoplayEnabled = slider.dataset.slider_autoplay === 'true';
+  const delay = Number(slider.dataset.slider_delay);
+
+  new Swiper(slider, {
+    speed: speed,
+    loop: true,
+    autoplay: autoplayEnabled
+      ? { delay: delay }
+      : false,
+    navigation: {
+      nextEl: slider.querySelector('.swiper-button-next'),
+      prevEl: slider.querySelector('.swiper-button-prev'),
+    }
+  });
+});
