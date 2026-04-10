6464f9fd4aa8169a4d252322f00d90cbb66d065d
ipanasiuk <ipanasiuk.1993@gmail.com>
2026-04-10 17:54:39 +0300

    Correct re-initialize swiper, destroy instance. Fix styles, add media queries

--- assets/custom.js
+++ assets/custom.js
@@ -1,21 +1,49 @@
-document.querySelectorAll('.announcement-bar__swiper').forEach((slider) => {
-  const speed = Number(slider.dataset.speed);
-  const autoplayEnabled = slider.dataset.autoplay;
-  const delay = Number(slider.dataset.delay);
+function initSwiper(container = document) {
+  const sliders = container.querySelectorAll('.announcement-bar__swiper');
 
-  new Swiper(slider, {
-    observer: true,
-    observeParents: true,  
-    speed: speed,
-    loop: true,
-    autoplay: autoplayEnabled
-      ? { delay: delay }
-      : false,
-    navigation: {
-      nextEl: slider.querySelector('.swiper-button-next'),
-      prevEl: slider.querySelector('.swiper-button-prev'),
+  sliders.forEach((slider) => {
+    if (slider.swiper) {
+      slider.swiper.destroy(true, true);
     }
+
+    const speed = Number(slider.dataset.speed);
+    const autoplayEnabled = slider.dataset.autoplay === 'true';
+    const delay = Number(slider.dataset.delay);
+
+    console.log(autoplayEnabled)
+
+    new Swiper(slider, {
+      observer: true,
+      observeParents: true,
+      speed,
+      loop: true,
+      autoplay: autoplayEnabled ? { delay } : false,
+      navigation: {
+        nextEl: slider.querySelector('.swiper-button-next'),
+        prevEl: slider.querySelector('.swiper-button-prev'),
+      },
+      breakpoints: {
+        0: {
+          navigation: {
+            enabled: false
+          }   
+        },
+        768: {
+          navigation: {
+            enabled: true
+          }          
+        }
+      }
+    });
   });
+}
+
+// initial load
+initSwiper();
+
+// Shopify editor
+document.addEventListener('shopify:section:load', (e) => {
+  initSwiper(e.target);
 });
 
 // Search button mobile
